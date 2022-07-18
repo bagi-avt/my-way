d0987380e466ef630a28a81c2624d91e991c96ae
Bagdaulet Abdumutallov <b.abdumutallov@globus-it.com>
2022-07-18 12:28:50 +0700

    fix routes pdf files

--- src/components/Popup/index.jsx
+++ src/components/Popup/index.jsx
@@ -80,12 +80,12 @@ const Popup = ({active, setActive, tariffActive, setTariffActive}) => {
                         <a
                             className='cursor'
                             target="_blank"
-                            href="http://test-front.ru/my-way/pdf/%D0%9F%D0%BE%D0%BB%D0%B8%D1%82%D0%B8%D0%BA%D0%B0%20%D0%BA%D0%BE%D0%BD%D1%84%D0%B8%D0%B4%D0%B5%D0%BD%D1%86%D0%B8%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8.pdf"
+                            href="https://mywayapp.ru/pdf/%D0%9F%D0%BE%D0%BB%D0%B8%D1%82%D0%B8%D0%BA%D0%B0%20%D0%BA%D0%BE%D0%BD%D1%84%D0%B8%D0%B4%D0%B5%D0%BD%D1%86%D0%B8%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8.pdf"
                         > Политики конфиденциальности</a>,
                         и <a
                         className='cursor'
                         target="_blank"
-                        href="http://test-front.ru/my-way/pdf/%D0%9F%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D1%81%D0%BA%D0%BE%D0%B5%20%D1%81%D0%BE%D0%B3%D0%BB%D0%B0%D1%88%D0%B5%D0%BD%D0%B8%D0%B5.pdf"
+                        href="https://mywayapp.ru/pdf/%D0%9F%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D1%81%D0%BA%D0%BE%D0%B5%20%D1%81%D0%BE%D0%B3%D0%BB%D0%B0%D1%88%D0%B5%D0%BD%D0%B8%D0%B5.pdf"
                     >Пользовательского соглашения</a>
                     </p>
                 </div>
